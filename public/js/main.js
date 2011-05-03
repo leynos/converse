7e36399e17ed85dbb71d3379b81f108eac1c08fc
Leynos <leynos@troubledskies.net>
2011-05-03 01:59:49 +0100

    Implementing feedback from Scottie

--- public/js/main.js
+++ public/js/main.js
@@ -48,6 +48,80 @@ Date.prototype.setISO8601 = function (string) {
     this.setTime(Number(time));
 };
 
+// Simulates PHP's date function
+// From http://jacwright.com/projects/javascript/date_format
+Date.prototype.format = function(format) {
+    var returnStr = '';
+    var replace = Date.replaceChars;
+    for (var i = 0; i < format.length; i++) {
+        var curChar = format.charAt(i);
+        if (i - 1 >= 0 && format.charAt(i - 1) == "\\") { 
+            returnStr += curChar;
+        }
+        else if (replace[curChar]) {
+            returnStr += replace[curChar].call(this);
+        } else if (curChar != "\\"){
+            returnStr += curChar;
+        }
+    }
+    return returnStr;
+};
+ 
+Date.replaceChars = {
+    shortMonths: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    longMonths: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    shortDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    longDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    
+    // Day
+    d: function() { return (this.getDate() < 10 ? '0' : '') + this.getDate(); },
+    D: function() { return Date.replaceChars.shortDays[this.getDay()]; },
+    j: function() { return this.getDate(); },
+    l: function() { return Date.replaceChars.longDays[this.getDay()]; },
+    N: function() { return this.getDay() + 1; },
+    S: function() { return (this.getDate() % 10 == 1 && this.getDate() != 11 ? 'st' : (this.getDate() % 10 == 2 && this.getDate() != 12 ? 'nd' : (this.getDate() % 10 == 3 && this.getDate() != 13 ? 'rd' : 'th'))); },
+    w: function() { return this.getDay(); },
+    z: function() { var d = new Date(this.getFullYear(),0,1); return Math.ceil((this - d) / 86400000); }, // Fixed now
+    // Week
+    W: function() { var d = new Date(this.getFullYear(), 0, 1); return Math.ceil((((this - d) / 86400000) + d.getDay() + 1) / 7); }, // Fixed now
+    // Month
+    F: function() { return Date.replaceChars.longMonths[this.getMonth()]; },
+    m: function() { return (this.getMonth() < 9 ? '0' : '') + (this.getMonth() + 1); },
+    M: function() { return Date.replaceChars.shortMonths[this.getMonth()]; },
+    n: function() { return this.getMonth() + 1; },
+    t: function() { var d = new Date(); return new Date(d.getFullYear(), d.getMonth(), 0).getDate() }, // Fixed now, gets #days of date
+    // Year
+    L: function() { var year = this.getFullYear(); return (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)); },   // Fixed now
+    o: function() { var d  = new Date(this.valueOf());  d.setDate(d.getDate() - ((this.getDay() + 6) % 7) + 3); return d.getFullYear();}, //Fixed now
+    Y: function() { return this.getFullYear(); },
+    y: function() { return ('' + this.getFullYear()).substr(2); },
+    // Time
+    a: function() { return this.getHours() < 12 ? 'am' : 'pm'; },
+    A: function() { return this.getHours() < 12 ? 'AM' : 'PM'; },
+    B: function() { return Math.floor((((this.getUTCHours() + 1) % 24) + this.getUTCMinutes() / 60 + this.getUTCSeconds() / 3600) * 1000 / 24); }, // Fixed now
+    g: function() { return this.getHours() % 12 || 12; },
+    G: function() { return this.getHours(); },
+    h: function() { return ((this.getHours() % 12 || 12) < 10 ? '0' : '') + (this.getHours() % 12 || 12); },
+    H: function() { return (this.getHours() < 10 ? '0' : '') + this.getHours(); },
+    i: function() { return (this.getMinutes() < 10 ? '0' : '') + this.getMinutes(); },
+    s: function() { return (this.getSeconds() < 10 ? '0' : '') + this.getSeconds(); },
+    u: function() { var m = this.getMilliseconds(); return (m < 10 ? '00' : (m < 100 ?
+'0' : '')) + m; },
+    // Timezone
+    e: function() { return "Not Yet Supported"; },
+    I: function() { return "Not Yet Supported"; },
+    O: function() { return (-this.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(this.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(this.getTimezoneOffset() / 60)) + '00'; },
+    P: function() { return (-this.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(this.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(this.getTimezoneOffset() / 60)) + ':00'; }, // Fixed now
+    T: function() { var m = this.getMonth(); this.setMonth(0); var result = this.toTimeString().replace(/^.+ \(?([^\)]+)\)?$/, '$1'); this.setMonth(m); return result;},
+    Z: function() { return -this.getTimezoneOffset() * 60; },
+    // Full Date/Time
+    c: function() { return this.format("Y-m-d\\TH:i:sP"); }, // Fixed now
+    r: function() { return this.toString(); },
+    U: function() { return this.getTime() / 1000; }
+};
+
+var en_GB_datef = 'D&\\n\\b\\s\\p;jS&\\n\\b\\s\\p;M&\\n\\b\\s\\p;Y, g:iA';
+
 // Replace quotes with an appropriate escape sequence to allow 
 // placing text in a js event
 function escapeQuotes(value) 
@@ -186,11 +260,13 @@ function ThreadUI(delegate)
         var onclick = function () {
             select(id, paper);
         };
+        var postDate = new Date();
+        postDate.setISO8601(post.date);
         circles[id] = c;
         c.attr("fill", "white");
         c.node.onclick = onclick;
         $(c.node).qtip({
-            content: 'id: '+post.id+'<br />author: '+users[post.author].displayname+'<br />date: '+post.date,
+            content: 'id: '+post.id+'<br />author: '+users[post.author].displayname+'<br />date: '+postDate.format(en_GB_datef),
             show: 'mouseover',
             hide: 'mouseout',
             position: {
@@ -268,6 +344,10 @@ function ThreadUI(delegate)
         div.dblclick(function(e) {
             select(post.id, paper);
         });
+
+        var postDate = new Date();
+        postDate.setISO8601(post.date);
+
         if (null !== post.body)
         {
             try {
@@ -283,15 +363,20 @@ function ThreadUI(delegate)
             div.prepend('<img src="user/'+encodeURIComponent(author_id)+
                 '/avatar" class="avatar" class="avatar" />');
         }
+        div.find('h3').before('<div class="date">'+ postDate.format(en_GB_datef)+'</div>');
 
         var messageToolbar = $('<div />', {
             'class': 'message-toolbar'
         });
         var deleteButton = $('<img />', {
             src: 'images/deletepost_s.png',
-            'class': 'message-control delete-button'
+            'class': 'message-control delete-button',
+            click: function() { window.alert('Not yet implemented'); }
         });
         messageToolbar.append(deleteButton);
+        if (post.author != delegate.username) {
+            deleteButton.hide();
+        }
         var replyButton = $('<img />', {
             src: 'images/reply_s.png',
             'class': 'message-control reply-button'
@@ -471,6 +556,7 @@ function ThreadUI(delegate)
         $("#messagepane").html("No post found! :o");
     };
 
+    $('#notepad').height($(window).height()*0.25);
     $("#messagepane").height( $(window).height() - $("#notepad").outerHeight() -8 );
 
     $('#notepad').resizable({
@@ -537,11 +623,11 @@ function BoardUI(delegate)
             htmlSpecialChars(thread.subject) + '</a></td><td>' + 
             htmlSpecialChars(thread.replies) + '</td><td>' +
             htmlSpecialChars(thread.newest_by) + ' (' +
-            newest.toLocaleDateString() + ')</td></tr>');
+            newest.format(en_GB_datef) + ')</td></tr>');
         $('#thread-table > tbody').append(
             '<tr class="byline ' + (odd?' odd':'') + '"><td colspan=3 ><small>' + 
             'Started by: ' + htmlSpecialChars(thread.started_by) + ' (' + 
-            started.toLocaleDateString() + ')</small></td></tr>');
+            started.format(en_GB_datef) + ')</small></td></tr>');
         odd = !odd;
     };
 
@@ -898,7 +984,8 @@ $(window.document).ready( function() {
     });
 
     $.get('loggedin', function(data) {
-        loggedin=data.loggedin;
+        converse.loggedin=data.loggedin;
+        converse.username=data.username;
         loadToolbar(data);
         $(window).trigger( 'hashchange' );
     });
